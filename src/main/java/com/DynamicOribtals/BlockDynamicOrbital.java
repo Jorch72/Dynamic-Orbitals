0f5cc8a77911316e0cc4c3b4e1c0c130fc0011a6
Alexander Mastrangelo <u0828016@utah.edu>
2017-05-06 15:03:49 -0600

    Setup the Workspace

---
+++ src/main/java/com/DynamicOribtals/BlockDynamicOrbital.java
@@ -0,0 +1,12 @@
+package com.DynamicOribtals;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockDynamicOrbital extends Block {
+
+	public BlockDynamicOrbital(Material materialIn) {
+		super(materialIn);
+	}
+
+}
---
+++ src/main/java/com/DynamicOribtals/TileEntityDynamicOrbital.java
@@ -0,0 +1,7 @@
+package com.DynamicOribtals;
+
+import net.minecraft.tileentity.TileEntity;
+
+public class TileEntityDynamicOrbital extends TileEntity {
+
+}
